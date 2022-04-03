ddcae1b7d8354116dc93f87968bf8c460f732bd2
Raiyan109 <kraiyan109@gmail.com>
2022-04-04 00:06:08 +0600

    header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h1>This is header </h1>
-            <Link to='/home'>Home </Link>
-            <Link to='/reviews'>Reviews </Link>
-            <Link to='/dashboard'>Dashboard </Link>
-            <Link to='/blogs'>Blogs </Link>
-            <Link to='/about'>About </Link>
-        </div>
+        <nav>
+
+            <Link className='link' to='/home'>Home </Link>
+            <Link className='link' to='/reviews'>Reviews </Link>
+            <Link className='link' to='/dashboard'>Dashboard </Link>
+            <Link className='link' to='/blogs'>Blogs </Link>
+            <Link className='link' to='/about'>About </Link>
+        </nav>
     );
 };
 
