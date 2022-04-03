00db4a6abf17ba9e49258c27e359915c36e69ae7
Raiyan109 <kraiyan109@gmail.com>
2022-04-03 23:50:46 +0600

    navbar raw done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
         <div>
-
+            <h1>This is header </h1>
+            <Link to='/home'>Home </Link>
+            <Link to='/reviews'>Reviews </Link>
+            <Link to='/dashboard'>Dashboard </Link>
+            <Link to='/blogs'>Blogs </Link>
+            <Link to='/about'>About </Link>
         </div>
     );
 };
